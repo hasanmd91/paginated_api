918300b82fbdb2f0f621edc830d07f10d0bd9198
hasanmd91 <hasanmd1991@hotmail.com>
2023-02-22 12:45:01 +0200

    update import statments

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const user = require("./users");
 const mongoose = require("mongoose");
